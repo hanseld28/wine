cf9d979e94fed87d4376b5c944e5094e03e05a3c
Hansel <hansel.donizete@gmail.com>
2020-06-23 21:31:01 -0300

    Add project template

---
+++ src/test/java/com/algaworks/wine/WineApplicationTests.java
@@ -0,0 +1,15 @@
+package com.algaworks.wine;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+public class WineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
